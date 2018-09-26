1ea38179a8e5158ac435235670b3a7b7abbebb04
dojutsu-user <vaibhgupt199@gmail.com>
2018-09-26 21:37:53 +0530

    Settings for deployment

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://api.themoviedb.org/3"
+  baseURL: "https://cors-anywhere.herokuapp.com/http://api.themoviedb.org/3"
 });
 
 export default AxiosInstance;
